0b74ba277d335ec0d79102fee87123de41e3e7c6
melisa-kabasakal <meliskabasakal33@gmail.com>
2026-01-14 22:40:13 +0300

    chore: init postgres with prisma

--- prisma/migrations/20260111180157_add_site_settings/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "SiteSettings" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "email" TEXT NOT NULL,
-    "phone" TEXT NOT NULL,
-    "address" TEXT NOT NULL
-);
--- prisma/migrations/20260111212219_/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "ContactMessage" ADD COLUMN "company" TEXT;
-ALTER TABLE "ContactMessage" ADD COLUMN "phone" TEXT;
