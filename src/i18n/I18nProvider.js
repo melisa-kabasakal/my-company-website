98af6c2b1e3ba7f7998c50361c0b08ec1ec9374c
melisa-kabasakal <meliskabasakal33@gmail.com>
2026-01-20 21:52:10 +0300

    Fix: disable client locale hook to fix build

--- src/i18n/I18nProvider.js
+++ src/i18n/I18nProvider.js
@@ -1,45 +1,16 @@
-"use client";
-
 import { NextIntlClientProvider } from "next-intl";
-import { createContext, useContext, useEffect, useState } from "react";
-import tr from "./messages/tr.json";
-import en from "./messages/en.json";
+import { getMessages } from "next-intl/server";
 
-const messages = { tr, en };
-
-const LocaleContext = createContext(null);
-
-export function useLocale() {
-  const ctx = useContext(LocaleContext);
-  if (!ctx) {
-    throw new Error("useLocale must be used inside I18nProvider");
-  }
-  return ctx;
-}
-
-export default function I18nProvider({ children }) {
-  const [locale, setLocale] = useState("tr");
-
-  useEffect(() => {
-    const savedLocale = localStorage.getItem("locale");
-    if (savedLocale === "tr" || savedLocale === "en") {
-      setLocale(savedLocale);
-    }
-  }, []);
-
-  const changeLocale = (nextLocale) => {
-    setLocale(nextLocale);
-    localStorage.setItem("locale", nextLocale);
-  };
+export default async function RootLayout({ children }) {
+  const messages = await getMessages();
 
   return (
-    <LocaleContext.Provider value={{ locale, changeLocale }}>
-      <NextIntlClientProvider
-        locale={locale}
-        messages={messages[locale]}
-      >
-        {children}
-      </NextIntlClientProvider>
-    </LocaleContext.Provider>
+    <html lang="tr">
+      <body>
+        <NextIntlClientProvider messages={messages}>
+          {children}
+        </NextIntlClientProvider>
+      </body>
+    </html>
   );
 }
