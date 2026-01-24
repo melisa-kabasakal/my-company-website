25bdd75aab9e271d2e32bc583e67319d86064bdb
melisa-kabasakal <meliskabasakal33@gmail.com>
2026-01-24 15:02:50 +0300

    Redirect vercel.app to custom domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'muntech.vercel.app',
+          },
+        ],
+        destination: 'https://www.muntechs.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
